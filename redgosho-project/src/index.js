727c76bc9d48e7b8e148fed66cf65c0b4587d67a
redgosho <redgosho@gmail.com>
2019-11-07 21:24:07 +0900

    タピオカドリンクグラフィック完成”

--- redgosho-project/src/index.js
+++ redgosho-project/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
-import App from './screens/App';
+import Title from './screens/Title';
 import * as serviceWorker from './screens/serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Title />, document.getElementById('root'));
 serviceWorker.unregister();
