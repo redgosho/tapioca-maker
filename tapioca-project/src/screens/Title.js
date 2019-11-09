e4e32e6508a2f85ea77d3479a3ecaf566a3244ab
redgosho <redgosho@gmail.com>
2019-11-09 11:38:09 +0900

    パーツデザイン調整（全体的に下に下げた）

--- tapioca-project/src/screens/Title.js
+++ tapioca-project/src/screens/Title.js
@@ -11,7 +11,9 @@ function Title() {
         <Tapioca__area>
           <Tapioca />
         </Tapioca__area>
-        <Enter>いちぐち</Enter>
+        <Enter__area>
+          <Enter>いりぐち</Enter>
+        </Enter__area>
         <IconButton__area>
           <InfoButton>
             <FontAwesomeIcon icon={['fas', 'info']} />
@@ -32,24 +34,35 @@ const Main__area = styled.div`
   background: rgba(255,0,0,0.5);
   border: 1px solid #aaa;
   position: relative;
+  flex-direction: column;
 `
 const Tapioca__area = styled.div`
+  // 縦中央寄せ
   position: absolute;
-  margin: auto;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  width: 0px;
-  height: 0px;
+  top: 60%;
+  left: 50%;
+  transform: translateY(-70%) translateX(-50%);
+  -webkit- transform: translateY(-70%) translateX(-50%);
 `
-
+const Enter__area = styled.div`
+  // 横中央寄せ
+  text-align: center;
+  // 縦中央寄せ
+  position: absolute;
+  top:75%;
+  left: 50%;
+  transform: translateY(-50%) translateX(-50%);
+  -webkit- transform: translateY(-50%) translateX(-50%);
+`
 const Enter = styled.button`
   display: inline-block;
-  padding: 0.5em 1em;
+  width: 200px;
+  font-size: 20px;
+  font-weight: bold;
+  padding: 0.1em 1em;
   text-decoration: none;
-  color: #67c5ff;
-  border: dashed 1px #67c5ff;
+  color: #1e90ff;
+  border: dashed 3px #1e90ff;
   background: #f2fcff;
   border-radius: 3px;
   transition: .4s;
@@ -60,20 +73,26 @@ const Enter = styled.button`
   }
 `
 const IconButton__area = styled.div`
+  // 横中央寄せ
   text-align: center;
+  // 縦中央寄せ
+  position: absolute;
+  top: 85%;
+  left: 50%;
+  transform: translateY(-50%) translateX(-50%);
+  -webkit- transform: translateY(-50%) translateX(-50%);
 `
 const InfoButton = styled.button`
   display: inline-block;
   text-decoration: none;
   color: rgba(152, 152, 152, 0.43);/*アイコン色*/
-  width: 80px;
-  height: 80px;
-  line-height: 80px;
-  font-size: 40px;
+  width: 40px;
+  height: 40px;
+  line-height: 0px;
+  font-size: 20px;
   border-radius: 50%;
   text-align: center;
   overflow: hidden;
-  font-weight: bold;
   background-image: linear-gradient(#e8e8e8 0%, #d6d6d6 100%);
   text-shadow: 1px 1px 1px rgba(255, 255, 255, 0.66);
   box-shadow: inset 0 2px 0 rgba(255,255,255,0.5), 0 2px 2px rgba(0, 0, 0, 0.19);
@@ -82,9 +101,6 @@ const InfoButton = styled.button`
   &:active {
     box-shadow: inset 0 1px 0 rgba(255,255,255,0.5), 0 2px 2px rgba(0, 0, 0, 0.19);
     border-bottom: none;
-  }
-  i {
-    line-height: 80px;
   }
 `
 
